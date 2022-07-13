ef9c0b7b75514e8d7c33326cb9a6aab54377b05e
patrizineversuxx <r2tr@yandex.ru>
2022-07-13 15:37:58 +0400

    Minor code changes

--- AspNetCoreTodo/Controllers/ToDoController.cs
+++ AspNetCoreTodo/Controllers/ToDoController.cs
@@ -13,6 +13,7 @@ namespace AspNetCoreTodo.Controllers
         {
             _todoItemService = todoItemService;
         }
+
         public async Task<IActionResult> Index()
         {
             var items = await _todoItemService.GetIncompleteItemsAsync();
