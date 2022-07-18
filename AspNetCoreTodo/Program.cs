564a6b06a91ac80f7276c09a4ed521efed28db67
patrizineversuxx <r2tr@yandex.ru>
2022-07-18 08:37:54 +0400

    Created Service class for DB

--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+builder.Services.AddScoped<ITodoItemService, TodoItemService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
